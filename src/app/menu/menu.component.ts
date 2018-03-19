616e040bb12b0ee7375c6e46996c8aacec67f325
thechitesh <thechitesh@gmail.com>
2018-03-19 13:45:11 +0100

    working till data binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -12,11 +12,15 @@ export class MenuComponent implements OnInit {
 
   dishes:Dish[] = DISHES; 
 
-  selectedDish:Dish = DISHES[0]; 
+  selectedDish:Dish; 
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(dish:Dish){
+    this.selectedDish = dish;
   }
 
 }
